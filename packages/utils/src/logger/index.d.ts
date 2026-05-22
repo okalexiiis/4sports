55879fbfe34411fc1ac96ae0022c2a01a5f87db0
Alexiiis <okalexiiis@gmail.com>
2026-05-22 15:22:17 -0700

    feat: add logger and result utility types, including domain error handling

---
+++ packages/utils/src/logger/index.d.ts
@@ -0,0 +1,4 @@
+export { createLogger } from './create';
+export type { Logger, LogLevel, LogMeta } from './types';
+export declare const logger: import("./types").Logger;
+//# sourceMappingURL=index.d.ts.map
